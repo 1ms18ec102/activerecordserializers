f2e6fb3a4b54b03f0f1bd0a987c95337e4e6a9e4
Savan  Awanti <savan.awanti@happiestminds.com>
2023-03-10 11:13:52 +0530

    syntax error in employee_serializer.rb

---
+++ db/seeds.rb
@@ -0,0 +1,27 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+Employee.destroy_all
+Manager.destroy_all
+
+managers = (1..20).map do
+  Manager.create!(
+    name: Faker::Name.name
+  )
+end
+
+employee = (1..50).map do
+  Employee.create!(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    dob: Faker::Date::birthday(min_age: 18, max_age: 65),
+    mobile: Faker::Number.number(digits: 10),
+    designation: Faker::Lorem.word,
+    salary: Faker::Number.positive,
+    manager: managers.sample
+  )
+end
